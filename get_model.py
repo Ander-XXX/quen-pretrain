2565858aed3a5ab30954d565941366125b3b0fdc
Ander-XXX <1721770724@qq.com>
2025-02-08 22:14:46 +0800

    2.8

--- get_model.py
+++ get_model.py
@@ -1,7 +1,7 @@
 from huggingface_hub import snapshot_download
 from huggingface_hub import login
 
-login(token = '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')
+login(token = '')
 snapshot_download(repo_id='Qwen/Qwen2.5-0.5B',
                   repo_type='model',
                   local_dir='./backbone/Qwen2.5-0.5B',
